8075cc575fe207c972daa292290814411e2f0385
Oleg <oleg.orlowsky@gmail.com>
2020-11-26 11:35:03 +0300

    Add queue

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace TwoWayQueueSort
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<int> a = new Queue<int>();
+            a.Enqueue(3);
+            a.Enqueue(-2);
+            a.Enqueue(10);
+
+            foreach (var x in a)
+            {
+                Console.Write(x + " ");
+            }
         }
     }
 }
