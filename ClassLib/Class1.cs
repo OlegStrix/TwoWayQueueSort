e168143731eb587f08ef82bef35e6ea6580b073c
Oleg <oleg.orlowsky@gmail.com>
2020-11-26 12:13:49 +0300

    Final version

--- ClassLib/Class1.cs
+++ ClassLib/Class1.cs
@@ -4,9 +4,110 @@ namespace ClassLib
 {
     public static class Class1
     {
+        public static ulong N_op { get; set; } = 0;
         public static void Sorting(Queue<int> a)
         {
-            Console.WriteLine("Add new function");
+            N_op++;
+            Queue<int> a1 = new Queue<int>();
+            Queue<int> a2 = new Queue<int>();
+            int i, j;
+            int k = 1;
+            N_op++;
+            while (k < a.Count)
+            {
+                N_op += 2;
+                while (a.Count != 0)
+                {
+                    N_op += 2;
+                    for (i = 0; i < k && a.Count != 0; i++)
+                    {
+                        N_op += 4 + 2 + 1;
+                        a1.Enqueue(a.Peek());
+                        a.Dequeue();
+                    }
+
+                    for (j = 0; j < k && a.Count != 0; j++)
+                    {
+                        N_op += 4 + 2 + 1;
+                        a2.Enqueue(a.Peek());
+                        a.Dequeue();
+                    }
+                }
+
+                while (a1.Count != 0 && a2.Count != 0)
+                {
+                    N_op += 4;
+                    i = 0;
+                    j = 0;
+                    N_op += 2;
+                    while (i < k && j < k && a1.Count != 0 && a2.Count != 0)
+                    {
+                        N_op += 6;
+                        if (a1.Peek() < a2.Peek())
+                        {
+                            N_op += 3;
+                            a.Enqueue(a1.Peek());
+                            N_op += 2;
+                            a1.Dequeue();
+                            N_op++;
+                            i++;
+                            N_op++;
+                        }
+                        else
+                        {
+                            a.Enqueue(a2.Peek());
+                            N_op += 2;
+                            a2.Dequeue();
+                            N_op++;
+                            j++;
+                            N_op++;
+                        }
+                    }
+
+                    while (i < k && a1.Count != 0)
+                    {
+                        N_op += 3;
+                        a.Enqueue(a1.Peek());
+                        N_op += 2;
+                        a1.Dequeue();
+                        N_op++;
+                        i++;
+                        N_op++;
+                    }
+
+                    while (j < k && a2.Count != 0)
+                    {
+                        N_op += 3;
+                        a.Enqueue(a2.Peek());
+                        N_op += 2;
+                        a2.Dequeue();
+                        N_op++;
+                        j++;
+                        N_op++;
+                    }
+                }
+
+                while (a1.Count != 0)
+                {
+                    N_op += 2;
+                    a.Enqueue(a1.Peek());
+                    N_op += 2;
+                    a1.Dequeue();
+                    N_op++;
+                }
+
+                while (a2.Count != 0)
+                {
+                    N_op += 2;
+                    a.Enqueue(a2.Peek());
+                    N_op += 2;
+                    a2.Dequeue();
+                    N_op++;
+                }
+
+                k *= 2;
+                N_op++;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using ClassLib;
 namespace TwoWayQueueSort
 {
@@ -8,11 +9,31 @@ namespace TwoWayQueueSort
         static void Main(string[] args)
         {
             Queue<int> a = new Queue<int>();
-            a.Enqueue(3);
-            a.Enqueue(-2);
-            a.Enqueue(1);
+            int f,p;
+            int DigitsCounts = 500000;
+            var time = new Stopwatch();
+            Random rand = new Random();
+            for (p = 0; p < 10; p++)
+            {
+                for (f = 0; f < DigitsCounts; f++)
+                {
+                    a.Enqueue(rand.Next(9999999));
+                }
+                time.Start();
+                Class1.Sorting(a);
+                time.Stop();
 
-            Class1.Sorting(a);
+                TimeSpan interval = TimeSpan.FromMilliseconds((double) time.ElapsedMilliseconds);
+                Console.WriteLine($"Number of sortings: {p + 1}");
+                Console.WriteLine($"\tNumber of sorted items: {DigitsCounts}");
+                Console.WriteLine($"\tSorting time (sec, ms): {interval.Seconds}, {interval.Milliseconds}");
+                Console.WriteLine($"\tN_op {Class1.N_op}\n");
+                
+                DigitsCounts += 500000;
+                Class1.N_op = 0;
+                a.Clear();
+                time.Reset();
+            }
         }
     }
 }
